49beee1f852499a14cc704475f14f5427ead40b9
alex222efx <47835055+alex222efx@users.noreply.github.com>
2019-02-21 19:10:11 +0200

    ipassign.sh
    This purpose of the script is to assign and bring up an additional IPs which are routed trough the main IP. The script works on Debian and Red Hat based operating systems.

---
+++ ipassign.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+
+
+#CentOS network config
+
+if [ -x "/usr/bin/yum" ]
+then
+echo
+echo
+echo Current Interfaces.....
+echo
+echo
+   ip a l
+###Obtain Variable Value#
+echo
+echo
+echo
+echo -e "Please chose the right logical name for the interface: \c"
+#
+read Interface
+#
+#
+#Create and fillup file###
+#
+touch /etc/sysconfig/network-scripts/ifcfg-$Interface
+    echo BOOTPROTO="static" >> /etc/sysconfig/network-scripts/ifcfg-$Interface
+    echo DEVICE="$Interface" >> /etc/sysconfig/network-scripts/ifcfg-$Interface
+    echo ONBOOT="yes" >> /etc/sysconfig/network-scripts/ifcfg-$Interface
+    echo IPV6INIT="no" >> /etc/sysconfig/network-scripts/ifcfg-$Interface
+
+
+#Define IP###
+#
+echo -e "Define IP: \c"
+#
+read IP
+#
+    echo IPADDR="$IP" >> /etc/sysconfig/network-scripts/ifcfg-$Interface
+#
+#Define Netmask###
+#
+   echo -e "Define Netmask: \c"
+#
+read Netmask
+#
+   echo NETMASK="$Netmask" >> /etc/sysconfig/network-scripts/ifcfg-$Interface
+#
+#Bring Up New Interface###
+#
+echo "Bringing up new interface..."
+#
+ifup $Interface
+#
+echo "Job Done....Exit"
+#
+#
+#
+#Debian Network Config###
+#
+#
+#
+elif [ -x "/usr/bin/apt-get" ]
+then
+#
+echo
+echo
+echo Current Interfaces....
+echo
+echo
+ip a l
+#
+#Obtain Variable Value###
+#
+echo
+echo
+echo -e "Please chose the right logical name for the interface: \c"
+#
+read Interface
+#
+#Append to /etc/network/interface###
+#
+echo auto $Interface >> /etc/network/interfaces
+echo iface $Interface inet static >> /etc/network/interfaces
+#
+echo -e "Define IP: \c"
+#
+read IP
+#
+echo address $IP >> /etc/network/interfaces
+#
+echo -e "define Netmask: \c"
+#
+read Netmask
+#
+echo natmask $Netmask >> /etc/network/interfaces
+#
+#Bringing up New Interface###
+#
+ifup Interface
+#
+echo "Job Done....Exit"
+#
+#If The Distribution is not Supported##
+else
+   echo "Unsupported Linux Distribution"
+   exit 1
+
+fi
