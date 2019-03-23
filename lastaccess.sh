7b9711ec9eb2e248fad5a9ec224c56cd67f363c4
alex222efx <47835055+alex222efx@users.noreply.github.com>
2019-03-23 16:40:45 +0200

    Update lastaccess.sh

--- lastaccess.sh
+++ lastaccess.sh
@@ -8,6 +8,5 @@ do
 #
  DT=$(stat $F | grep Access | tail -1 | cut -d " " -f2)
 #
-    echo " $F |  $(du -b $F | cut -f1)  bytes | accessed  $DT"
+    echo " $F | $(du -b $F | cut -f1) bytes | accessed  $DT"
 done
-
